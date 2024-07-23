4985b60fd348173f956451650eb0387a9e5962c9
SANA AMMAR <sanataffor@gmail.com>
2024-07-23 21:05:55 +0300

    save to local storage

--- script.js
+++ script.js
@@ -1,20 +1,88 @@
 // add a task function 
+// create tasksarray
+// save tasksarray to local storage
+// check if the task array is empty
+// if the task array is not empty push the content to the task array and update the interface
+
+
 // mark as done function
 // clear completed function
 // filtiration (active,compleated,all)
 // dynamic text showing how many tasks left
-// import  $ from 'jquery'
+
 const body = $("body")
 const todoElement = $("#todo-element") 
 const group = $(".group")
 const checkedItems = $(".task:checked");
-
+const active = $(".active-filter")
+const completed = $(".completed-filter")
+const all = $(".all-filter")
 let itemNum = 2;
+let tasksArray = []
+
+getFromLocal()
+if(localStorage.getItem("tasks")){
+    tasksArray = JSON.parse(localStorage.getItem("tasks"))
+    let titles = tasksArray.map(task => task.title);
+    console.log(titles)
+
+    addTask(titles)
+
+    
+}
+
+
 
 // function to add tasks 
-function addTask(){
+function addTaskToArray(tasktext){
+    let taskObj = {
+        "id": Date.now(),
+        "title": tasktext,
+        "isCompleted": false
+    }
+    tasksArray.push(taskObj)
+
+}
+function addTaskToJson(){
+    let stringTask = JSON.stringify(tasksArray)
+    window.localStorage.setItem("tasks",stringTask)
+   
+}
+function getFromLocal(){
+    const data = window.localStorage.getItem("tasks")
+    if(data){
+        const tasks = JSON.parse(data)
+       
+    }
+
+}
+function deletPrev(){
+    document.querySelectorAll(".list-group-item").forEach((old) =>{
+        old.remove()
+    })
+}
+function addTask(prevTask =""){
+
+if (prevTask != ""){
+        prevTask.forEach((task) =>{
+           
+        let dynamiclist = $("<li></li>")
+        dynamiclist.addClass("list-group-item")
+        let input = $("<input>")
+        input.attr("id", `item${itemNum}`);
+        input.addClass("task")
+        input.attr("type","checkbox")
+        let label = $("<label></label>")
+        label.attr("for",`item${itemNum}`)
+        label.text(task)
     
-    let task = $("input").val()
+        dynamiclist.append(input)
+        dynamiclist.append(label)
+        group.append(dynamiclist)
+        addTaskToArray(task)})
+}
+    
+    let task = $("input").val() 
     let dynamiclist = $("<li></li>")
     dynamiclist.addClass("list-group-item")
     let input = $("<input>")
@@ -28,10 +96,14 @@ function addTask(){
     dynamiclist.append(input)
     dynamiclist.append(label)
     group.append(dynamiclist)
+    
+    addTaskToArray(task)
     markAsCompleted();
+    addTaskToJson()
 
     
 }
+
 
 function clearCompleted(){
     const warapper = document.querySelectorAll(".list-group-item")
@@ -51,9 +123,7 @@ function taskNum(){
 
 }
 
-const Completed =$(".clear-completed")
-
-
+const clearcompleted =$(".clear-completed")
 const tasks = document.querySelectorAll(".task")
 function markAsCompleted(){
     const warapper = document.querySelectorAll(".list-group-item");
@@ -71,17 +141,16 @@ function markAsCompleted(){
 }
 
 
-
-
-
 const button = $(".btn-outline-secondary")
 button.on("click",function(){
+    deletPrev()
     addTask()
     itemNum ++
 
 })
 markAsCompleted()
-Completed.on("click",function(){
+
+clearcompleted.on("click",function(){
     clearCompleted().forEach(function(taskcompleted){
         taskcompleted.remove()
     })
@@ -90,4 +159,3 @@ Completed.on("click",function(){
 })
 
 
-console.log(taskNum())
